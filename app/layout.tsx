76db64f785c684fbce15a013c5e9f228e3877c45
nazmul.sourab <nazmul.sourab@technonext.com>
2025-04-10 02:47:26 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
           <AuthProvider>
             <Header />
             {children}
+            <SpeedInsights />
           </AuthProvider>
         </ThemeProvider>
       </body>
